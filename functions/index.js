d9c513b5cdd405143af2108dd41914d7a6a42e86
Jacob <jpeterson976@gmail.com>
2020-04-01 22:24:01 -0400

    Updated function to use Cloud Firestore

--- functions/index.js
+++ functions/index.js
@@ -7,21 +7,27 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-var admin = require("firebase-admin");
+var admin = require('firebase-admin');
 
-var serviceAccount = require("./google-services.json");
+var serviceAccount = require('./google-services.json');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://sd1-backend.firebaseio.com"
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://sd1-backend.firebaseio.com'
 });
 
-exports.saveFileName = functions.storage.object()
-    .onFinalize((snapshot, context) => {
-        // console.log(`Snapshot: ${snapshot}`);
-        // console.log(`Context: ${context}`);
-        // const data = snapshot.name;
+exports.saveFileName = functions.storage.object().onFinalize(async object => {
+	let start = object.name.lastIndexOf('/') + 1;
 
-        // return admin.firestore().collection('filenames').doc().set({ name: snapshot.name })
-        return admin.database().ref('filenames').push(snapshot.name);
-    });
+	let filename = object.name.substring(start, object.name.length);
+	let uploadDate = new Date(Date.now());
+
+	return admin
+		.firestore()
+		.collection('metadata')
+		.doc()
+		.set({
+			date: uploadDate,
+			name: filename
+		});
+});
