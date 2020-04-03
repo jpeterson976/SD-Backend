f7c6ae1a34d9d78c37685455462a6b75fe22d0ce
Jacob <jpeterson976@gmail.com>
2020-04-03 12:24:59 -0400

    Updated to follow new database schema

--- functions/index.js
+++ functions/index.js
@@ -16,18 +16,84 @@ admin.initializeApp({
 	databaseURL: 'https://sd1-backend.firebaseio.com'
 });
 
+//
 exports.saveFileName = functions.storage.object().onFinalize(async object => {
-	let start = object.name.lastIndexOf('/') + 1;
+	let divider = object.name.lastIndexOf('/');
 
-	let filename = object.name.substring(start, object.name.length);
+	let filename = object.name.substring(divider + 1);
+	let ext = object.name.substring(object.name.lastIndexOf('.'));
 	let uploadDate = new Date(Date.now());
+	let uuid = object.name.substring(0, divider);
 
-	return admin
-		.firestore()
-		.collection('metadata')
-		.doc()
-		.set({
-			date: uploadDate,
-			name: filename
-		});
+	if (ext === '.obj') {
+		return admin
+			.firestore()
+			.collection('metadata')
+			.doc(uuid)
+			.set(
+				{
+					OBJname: filename,
+					timestamp: uploadDate,
+					simComplete: false
+				},
+				{ merge: true }
+			);
+	} else if (ext === '.mtl') {
+		return admin
+			.firestore()
+			.collection('metadata')
+			.doc(uuid)
+			.set(
+				{
+					MTLname: filename,
+					timestamp: uploadDate,
+					simComplete: false
+				},
+				{ merge: true }
+			);
+	} else if (ext === '.png') {
+		if (filename.includes('_top')) {
+			return admin
+				.firestore()
+				.collection('metadata')
+				.doc(uuid)
+				.set(
+					{
+						PNGnames: {
+							top: filename
+						},
+						simComplete: true
+					},
+					{ merge: true }
+				);
+		} else if (filename.includes('_bottom')) {
+			return admin
+				.firestore()
+				.collection('metadata')
+				.doc(uuid)
+				.set(
+					{
+						PNGnames: {
+							bottom: filename
+						},
+						simComplete: true
+					},
+					{ merge: true }
+				);
+		} else if (filename.includes('_hand')) {
+			return admin
+				.firestore()
+				.collection('metadata')
+				.doc(uuid)
+				.set(
+					{
+						PNGnames: {
+							hand: filename
+						},
+						simComplete: true
+					},
+					{ merge: true }
+				);
+		}
+	}
 });
